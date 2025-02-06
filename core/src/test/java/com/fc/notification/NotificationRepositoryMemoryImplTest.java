910bbf3773325e693db6f97194f19d9aa0ed6845
JustHo <zalop3128@kakao.com>
2025-02-06 21:35:33 +0900

    [FEAT] MongoDB Config 작성 및 Test 코드 수정

--- core/src/test/java/com/fc/notification/NotificationRepositoryMemoryImplTest.java
+++ core/src/test/java/com/fc/notification/NotificationRepositoryMemoryImplTest.java
@@ -2,24 +2,31 @@ package com.fc.notification;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@SpringBootApplication
 class NotificationRepositoryMemoryImplTest {
-    private final NotificationRepositoryMemoryImpl sut = new NotificationRepositoryMemoryImpl();
+
+    @Autowired
+    private NotificationRepository sut;
+
     private final Instant now = Instant.now();
-    private final Instant deletedAt = Instant.now().plus(90, DAYS);
+    private final Instant ninetyDaysAfter = Instant.now().plus(90, DAYS);
 
 
     @DisplayName("알림 객체를 저장")
     @Test
     void test_save() {
-        sut.save(new Notification("1", 2L, NotificationType.LIKE, now, deletedAt));
+        sut.save(new Notification("1", 2L, NotificationType.LIKE, now, ninetyDaysAfter));
 
         Optional<Notification> notification = sut.findById("1");
 
@@ -29,7 +36,7 @@ class NotificationRepositoryMemoryImplTest {
     @DisplayName("저장후 조회")
     @Test
     void test_find_by_id() {
-        sut.save(new Notification("2", 2L, NotificationType.COMMENT, now, deletedAt));
+        sut.save(new Notification("2", 2L, NotificationType.COMMENT, now, ninetyDaysAfter));
 
         Optional<Notification> optionalNotification = sut.findById("2");
 
@@ -37,8 +44,8 @@ class NotificationRepositoryMemoryImplTest {
         assertEquals(notification.id, "2");
         assertEquals(notification.userId, 2L);
         assertEquals(notification.notificationType, NotificationType.COMMENT);
-        assertEquals(notification.createdAt, now);
-        assertEquals(notification.deletedAt, deletedAt);
+        assertEquals(notification.createdAt.getEpochSecond(), now.getEpochSecond());
+        assertEquals(notification.deletedAt.getEpochSecond(), ninetyDaysAfter.getEpochSecond());
     }
 
 
@@ -46,7 +53,7 @@ class NotificationRepositoryMemoryImplTest {
     @DisplayName("저장 후 삭제")
     @Test
     void test_delete_by_id() {
-        sut.save(new Notification("3", 3L, NotificationType.COMMENT, now, deletedAt));
+        sut.save(new Notification("3", 3L, NotificationType.COMMENT, now, ninetyDaysAfter));
         sut.deleteById("3");
 
         Optional<Notification> optionalNotification = sut.findById("3");
