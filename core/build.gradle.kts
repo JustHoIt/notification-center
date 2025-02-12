23821e7a2d5eceec0e25ce9beca1b9708a04cd8f
JustHo <zalop3128@kakao.com>
2025-02-13 03:37:17 +0900

    [FEAT] 코어 모듈 Redis Configuration 작성 1. 코어 모듈 Redis 의존성 추가 2. Redis Configuration 작성

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.testcontainers:testcontainers:1.19.8")
+    api("org.springframework.cloud:spring-cloud-starter-stream-kafka")
 }
