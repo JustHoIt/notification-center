77e00acf857ffa9345a970bb3fd3a75f74219cb7
JustHo <zalop3128@kakao.com>
2025-02-13 23:27:34 +0900

    [FEAT] DB local, test 환경 분리

--- core/src/main/java/com/fc/config/mongo/LocalMongoConfig.java
+++ core/src/main/java/com/fc/config/mongo/LocalMongoConfig.java
@@ -1,49 +1,19 @@
 package com.fc.config.mongo;
 
 import com.mongodb.ConnectionString;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.utility.DockerImageName;
 
+@Profile("local")
 @Configuration
-@Slf4j
 public class LocalMongoConfig {
 
-    private static final String MONGODB_IMAGE_NAME = "mongo:5.0";
-    private static final int MONGODB_INNER_PORT = 27017;
+    private static final String MONGO_HOST = "localhost";
+    private static final int MONGO_PORT = 27017;
     private static final String DATABASE_NAME = "notification";
-    private static final GenericContainer mongo = createMongoInstance();
-
-    private static GenericContainer createMongoInstance() {
-        return new GenericContainer(DockerImageName.parse(MONGODB_IMAGE_NAME))
-                .withExposedPorts(MONGODB_INNER_PORT)
-                .withReuse(true);
-    }
-
-    @PostConstruct
-    public void startMongo() {
-        try {
-            mongo.start();
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-
-    @PreDestroy
-    public void stopMongo() {
-        try {
-            mongo.stop();
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-
 
     @Bean(name = "notificationMongoFactory")
     public MongoDatabaseFactory notificationMongoFactory() {
@@ -51,10 +21,7 @@ public class LocalMongoConfig {
     }
 
     private ConnectionString connectionString() {
-        String host = mongo.getHost();
-        Integer port = mongo.getMappedPort(MONGODB_INNER_PORT);
 
-        return new ConnectionString("mongodb://" + host + ":" + port + "/" + DATABASE_NAME);
+        return new ConnectionString("mongodb://" + MONGO_HOST + ":" + MONGO_PORT + "/" + DATABASE_NAME);
     }
-
 }
